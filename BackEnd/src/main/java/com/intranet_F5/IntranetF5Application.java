19d6f6458042a3df82c4d3c9858edeef919580f2
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-09-15 13:12:32 +0200

    BackEnd schema

---
+++ BackEnd/src/main/java/com/intranet_F5/IntranetF5Application.java
@@ -0,0 +1,14 @@
+package com.intranet_F5;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetF5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetF5Application.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/intranet_F5/IntranetF5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intranet_F5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetF5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
