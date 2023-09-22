63530b0e422474ec381b8850c98d3a64d8e9d044
albegosu <albegosu@gmail.com>
2023-09-22 15:43:04 +0200

    Estructura basica nav

--- FrontEnd/src/views/AuthoriserCheckView.vue
+++ FrontEnd/src/views/AuthoriserCheckView.vue
@@ -1,15 +1,7 @@
 <script setup>
-import NavbarComponent from '../components/NavbarComponent.vue'
-import CalendarComponent from '../components/CalendarComponent.vue'
-import FooterComponent from '../components/FooterComponent.vue';
-
 
 </script>
 
 <template>
-  <main>
-    <NavbarComponent />
-    <CalendarComponent/>
-    <FooterComponent />
-  </main>
+  <div></div>
 </template>
--- FrontEnd/src/views/WorkerView.vue
+++ FrontEnd/src/views/WorkerView.vue
@@ -1,17 +1,6 @@
 <script setup>
-import NavbarComponent from '../components/NavbarComponent.vue'
-import CalendarComponent from '../components/CalendarComponent.vue'
-import FooterComponent from '../components/FooterComponent.vue';
-import CardInfoComponent from '../components/CardInfoComponent.vue';
-
-
 </script>
 
 <template>
-  <main>
-    <NavbarComponent />
-    <CardInfoComponent />
-    <CalendarComponent/>
-    <FooterComponent />
-  </main>
+  <div></div>
 </template>
