0ef9295341ac60c75b47292bd4e47170ca6a0a9c
LuLu <lu.luzan@gmail.com>
2023-09-23 20:16:14 +0200

    HABEMUS_CALENDARIO :)

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <script setup>
 import NavbarComponent from '../components/NavbarComponent.vue'
 import FooterComponent from '../components/FooterComponent.vue';
-import CalendarComponent from '../components/CalendarComponent.vue'
+// import CalendarComponent from '../components/CalendarComponent.vue'
+import CalendarHolidaysComponent from '../components/CalendarHolidaysComponent.vue';
 
 </script>
 
 <template>
   <main>
     <NavbarComponent />
-    <CalendarComponent/>
+    <!-- <CalendarComponent/> -->
+    <CalendarHolidaysComponent/>
     <FooterComponent />
   
   </main>
