693139587d5d998fe46546db28bf4ba191200ab8
LuLu <lu.luzan@gmail.com>
2023-09-25 12:46:33 +0200

    RenamingViews + HomeBanner

--- FrontEnd/src/views/HomeView.vue
+++ FrontEnd/src/views/HomeView.vue
@@ -1,17 +1,37 @@
 <script setup>
-import CalendarHolidaysComponent from '../components/CalendarHolidaysComponent.vue';
+// import CalendarHolidaysComponent from '../components/CalendarHolidaysComponent.vue';
 
 </script>
 
 <template>
-    
+    <div id="banner">
+        <img src="/img_banner.png" alt="networking">
+        <h1>¡Somos <span class="destacat">ágiles, creativos/as</span> y comprometidos/as!</h1>
+    </div>
     <!-- <CalendarComponent/> -->
-    <CalendarHolidaysComponent/>
+    <!-- <CalendarHolidaysComponent/> -->
 </template>
 
-<style>
+<style scoped>
 /* PRUEBA PARA COLOCACIÓN FOOTER */
 main {
     height: 900rem;
+}
+
+#banner {
+    position: relative;
+    text-align: center;
+    color: #000;
+    text-align: center;
+    font-family: Poppins;
+    font-size: 2.5rem;
+    font-style: normal;
+    font-weight: normal; 
+    line-height: normal;
+    background-color: #FF4700;
+    margin-bottom: 2rem;
+}
+.destacat{
+    font-weight:bolder;
 }
 </style>
