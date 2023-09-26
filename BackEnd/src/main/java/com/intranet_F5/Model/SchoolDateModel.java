4281eb4789b9d910bba133c4fb60db720ce0b342
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-09-26 14:20:02 +0200

    OK SchoolDateModel doesn't give id to JSON

--- BackEnd/src/main/java/com/intranet_F5/Model/SchoolDateModel.java
+++ BackEnd/src/main/java/com/intranet_F5/Model/SchoolDateModel.java
@@ -1,5 +1,7 @@
 package com.intranet_F5.Model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +21,7 @@ import java.time.LocalDate;
 public class SchoolDateModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonBackReference
     private Long id;
 
     private LocalDate date;
