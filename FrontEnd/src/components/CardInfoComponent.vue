5addcc3e1a2b5cae0262ecd652bfb1178392f37d
javier <ertoyos@gmail.com>
2023-09-26 11:26:03 +0200

    add card-info

--- FrontEnd/src/components/CardInfoComponent.vue
+++ FrontEnd/src/components/CardInfoComponent.vue
@@ -1,47 +1,48 @@
 <script setup>
-import axios from 'axios'
+import { ref } from 'vue';
 import { VCard, VCardTitle, VCardSubtitle, VCardText } from "vuetify/dist/vuetify.min";
+import CardInfoService from '../services/CardInfoService';
 
-const image = ref(null);
+const data = ref(null);
 
-async function getImage() {
-  const response = await axios.get(props.data.image);
-  image.value = response.data;
+async function getWorkerData() {
+  const response = await CardInfoService.getWorkerData(props.id);
+  data.value = response.data;
 }
 
-getImage();
+getWorkerData();
 </script>
 
 <template>
   <v-card>
     <v-card-title>
       <v-avatar size="60">
-        <v-img :src="image" />
+        <v-img :src="data.value.image" />
       </v-avatar>
       <v-card-subtitle>
-        {{ data.name }}
+        {{ data.value.name }}
       </v-card-subtitle>
     </v-card-title>
     <v-card-text>
       <v-row>
         <v-col cols="6">
-          <strong>Puesto:</strong> {{ data.position }}
+          <strong>Puesto:</strong> {{ data.value.position }}
         </v-col>
         <v-col cols="6">
-          <strong>Fecha de inicio:</strong> {{ data.start_date }}
+          <strong>Fecha de inicio:</strong> {{ data.value.start_date }}
         </v-col>
       </v-row>
       <v-row>
         <v-col cols="6">
-          <strong>Departamento:</strong> {{ data.department }}
+          <strong>Departamento:</strong> {{ data.value.department }}
         </v-col>
         <v-col cols="6">
-          <strong>Escuela de procedencia:</strong> {{ data.school }}
+          <strong>Escuela de procedencia:</strong> {{ data.value.school }}
         </v-col>
       </v-row>
       <v-row>
         <v-col cols="12">
-          <strong>Supervisor:</strong> {{ data.supervisor }}
+          <strong>Supervisor:</strong> {{ data.value.supervisor }}
         </v-col>
       </v-row>
     </v-card-text>
