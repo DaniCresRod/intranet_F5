77b76b97e3bc11a45396b4aa941bbf2c817ec035
LuLu <lu.luzan@gmail.com>
2023-09-28 10:01:58 +0200

    Styles_done

--- FrontEnd/src/views/HrGeneral.vue
+++ FrontEnd/src/views/HrGeneral.vue
@@ -20,11 +20,11 @@ import CardInfoComponent from '../components/CardInfoComponent.vue'
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    margin: 5rem;
 }
 
 .item {
-    width: calc(50% - 10px);
-    /* El 10px es el espacio entre elementos */
+    width: calc(50% - 4rem);
     height: 5.625rem;
     background-color: #FBF5F5;
     display: flex;
@@ -35,8 +35,10 @@ import CardInfoComponent from '../components/CardInfoComponent.vue'
     font-size: 0.9375rem;
     font-weight: 400;
     border: 1px solid var(--orange);
-    margin-bottom: 10px;
+    margin-bottom: 4rem;
     box-sizing: border-box;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
+
 }
 
 .item:last-child {
