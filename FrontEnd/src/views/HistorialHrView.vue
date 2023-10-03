256270f4e33b2f3ce4ad3f7eba283c13ae902f52
LuLu <lu.luzan@gmail.com>
2023-10-03 14:23:55 +0200

    historial_hr_view

---
+++ FrontEnd/src/views/HistorialHrView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import NewCardInfoComponent from '../components/NewCardInfoComponent.vue';
+import RequestListComponent from '../components/RequestListComponent.vue';
+
+</script>
+<template>
+    <NewCardInfoComponent/>
+    <RequestListComponent/>
+</template>
