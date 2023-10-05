8f17aac75e881874bf6c21ce7aed750f4e29bfcb
Dani <dani.crespo.rodriguez@gmail.com>
2023-10-05 09:42:18 +0200

    wip dev merge

--- BackEnd/src/main/java/com/intranet_F5/Config/WebSecurityConfig.java
+++ BackEnd/src/main/java/com/intranet_F5/Config/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.intranet_F5.Config;
 
+/**
+ * WebSecurityConfig se encarga de crear la estructura de filtros que se encontraran las
+ * peticiones antes de llegar a los controladores (Servlet)
+ */
 
 import com.intranet_F5.Model.UserModel;
 import com.intranet_F5.Repository.UserRepository;
@@ -12,19 +16,8 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
-import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.antlr.v4.runtime.atn.SemanticContext.and;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -38,29 +31,6 @@ public class WebSecurityConfig{
     @Autowired
     UserRepository userRepository;
 
-    /**
-     * https://medium.com/@davidbernalgonzalez/la-biblia-de-spring-security-635f35bb1a26
-     * @HttpSecurity: es equivalente a trabajar con un fichero XML en los que
-     *                definir la seguridad de las peticiones. Por tanto, esta clase
-     *                sería la alternativa mediante a la cual vamos a realizar la
-     *                configuración.
-     * @authorizeHttpRequests: nos permite autorizar a la petición HTTP, es decir, a
-     *                         la HTTP request
-     * @anyRequest: mediante el método anterior ya hemos autorizado la request, en
-     *              este caso en concrecto, vamos a autorizar a todas las request,
-     *              ya que con este metodo especificamos any request (cualquier
-     *              request).
-     * @authentificated: el usuario debe de estar autentificado.
-     * @httpBasic: nos permite agregar una instancia a la cadena de filtros en la
-     *             que se añadirá el filtro de BasicAutheticationFilter, que como ya
-     *             hemos explicado, es el encargado de realizar una autentificación
-     *             básica de la manera más típica, con una ventana de login, user y
-     *             password. Cuando añadimos este método, la autentificación
-     *             intentará realizar el logeo y cuando este sea satisfactorio
-     *             guardará el objeto de autentificación resultante en el
-     *             SegurityContextHolder para que posteriormente podamos consultar
-     *             dicha autentificación en el futuro.
-     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -70,7 +40,6 @@ public class WebSecurityConfig{
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/schools").hasRole("Supervisor")
                         .anyRequest().authenticated()
-
                 )
                 .httpBasic(withDefaults());
 //        http
@@ -89,28 +58,37 @@ public class WebSecurityConfig{
                 //.failureUrl("/login?error=true")
                 .permitAll());
 
-
         return http.build();
-
-
-//            return http
-//                    .csrf(csrf ->
-//                            csrf.disable())
-//                    .authorizeHttpRequests(authRequest ->
-//                            authRequest
-//                                    .requestMatchers("/auth/**").permitAll()
-////                                    .requestMatchers("/request/**").permitAll()
-////                                    .requestMatchers("/users/**").permitAll()
-////                                    .requestMatchers("/**").permitAll()
-//                                    //.requestMatchers("/request/**").permitAll()
-//                                    .anyRequest().authenticated()
-//                    )
-//                    .sessionManagement(sessionManager->
-//                        sessionManager
-//                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                            .authenticationProvider(authenticationProvider)
-//                            .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
-//
-//                    .build();
     }
 }
+
+/**
+ * https://medium.com/@davidbernalgonzalez/la-biblia-de-spring-security-635f35bb1a26
+ * @HttpSecurity: es equivalente a trabajar con un fichero XML en los que
+ *                definir la seguridad de las peticiones. Por tanto, esta clase
+ *                sería la alternativa mediante a la cual vamos a realizar la
+ *                configuración.
+ * @authorizeHttpRequests: nos permite autorizar a la petición HTTP, es decir, a
+ *                         la HTTP request
+ * @anyRequest: mediante el método anterior ya hemos autorizado la request, en
+ *              este caso en concrecto, vamos a autorizar a todas las request,
+ *              ya que con este metodo especificamos any request (cualquier
+ *              request).
+ * @authentificated: el usuario debe de estar autentificado.
+ * @httpBasic: nos permite agregar una instancia a la cadena de filtros en la
+ *             que se añadirá el filtro de BasicAutheticationFilter, que como ya
+ *             hemos explicado, es el encargado de realizar una autentificación
+ *             básica de la manera más típica, con una ventana de login, user y
+ *             password. Cuando añadimos este método, la autentificación
+ *             intentará realizar el logeo y cuando este sea satisfactorio
+ *             guardará el objeto de autentificación resultante en el
+ *             SegurityContextHolder para que posteriormente podamos consultar
+ *             dicha autentificación en el futuro.
+ *
+ * @requestMatchers: establece la ruta de la API sobre la que se se va a actuar, es decir,
+ *                  que queremos que suceda cuando se quiera acceder a determinada ruta:
+ *                      .permitAll      -> Que a esa ruta pueda entrar cualquiera
+ *                      .hasRole        -> Que a esa ruta solo pueda entrar alguien con determinado
+ *                                          rol (Autorizacion).
+ *                      .authenticated  ->  Solo usuarios autenticados
+ */
