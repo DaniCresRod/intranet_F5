94f9903cca4b303ed2de714c45d5f554d5fc006e
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-10-10 22:29:42 +0200

    OK consolelog out and comments maked up. pulled origin dev

--- FrontEnd/src/services/LogInService.js
+++ FrontEnd/src/services/LogInService.js
@@ -3,15 +3,12 @@ import http from '../httpCommon';
 export default {
   
   async doLogIn(payload) {
-    try{
-      console.log(JSON.stringify(payload));
-        
+    try{        
         let myToken=await http.post("/auth/login", JSON.stringify(payload));
-        console.log(myToken.data);
 
-        window.localStorage.setItem("myToken_Key", myToken.data.token.toString());
-        
+        window.localStorage.setItem("myToken_Key", myToken.data.token.toString());        
         window.localStorage.setItem("myUser_Key", myToken.data.userId.toString());
+
         return myToken.data.token;
     }
     catch(error){
@@ -22,10 +19,8 @@ export default {
 
   async whereToGo(){
     let userId=window.localStorage.getItem("myUser_Key");
-    console.log(userId);
     let myUser=await http.get(`/users/${userId}`);
-    console.log(myUser.data);
-
+    
     return myUser.data.userType;
   }
 };
