e8bfc03612ab3165a4a07bdd6cb2d9c0932bd183
LuLu <lu.luzan@gmail.com>
2023-10-13 12:14:10 +0200

    .

--- FrontEnd/src/components/NavbarComponent.vue
+++ FrontEnd/src/components/NavbarComponent.vue
@@ -2,17 +2,17 @@
 import { RouterLink } from 'vue-router';
 import { ref, onMounted } from 'vue'
 
-const userId=ref();
+const userId = ref();
 userId.value = window.localStorage.getItem("myUser_Key");
 
-function LogOut(){
+function LogOut() {
   window.localStorage.clear();
 }
 
 //Funcion que define lo que pasara si hay cambios
 function handleMutations(mutationsList) {
   for (const mutation of mutationsList) {
-    if (mutation.type === 'childList') {      
+    if (mutation.type === 'childList') {
       //console.log('Se ha producido un cambio en el DOM.');
       userId.value = window.localStorage.getItem("myUser_Key");
     }
@@ -33,14 +33,13 @@ onMounted(() => {
 </script>
 
 <template>
-  
   <header class="header">
     <ul class="iconsGroup">
       <li>
-  <a href="https://www.linkedin.com/company/factor%C3%ADaf5/" target="_blank">
-    <img src="/public/logo-linkedin.png">
-  </a>
-</li>
+        <a href="https://www.linkedin.com/company/factor%C3%ADaf5/" target="_blank">
+          <img src="/public/logo-linkedin.png">
+        </a>
+      </li>
       <li>
         <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Ffactoriaf5" target="_blank">
           <img src="/public/logo-twitter.png">
@@ -64,7 +63,7 @@ onMounted(() => {
     </ul>
     <nav class="navbar">
       <img src="/public/logo_f5.png" class="img-fluid navbar__img" alt="Logo Factoria F5">
-      
+
       <ul class="navbar__menu">
         <li class="navbar__menu--item">
           <a href="https://factoriaf5.org/" target="_blank">HOME</a>
@@ -84,31 +83,32 @@ onMounted(() => {
         <li class="navbar__menu--item">
           <a href="https://factoriaf5.org/contacto/" target="_blank">CONTACTO</a>
         </li>
-        <li v-if="userId!==''" class="navbar__menu--item" @click="LogOut()">
+        <li v-if="userId !== ''" class="navbar__menu--item" @click="LogOut()">
           <RouterLink to="/">LOGOUT</RouterLink>
         </li>
         <li v-else class="navbar__menu--item">
           <RouterLink to="/">LOGIN</RouterLink>
         </li>
-        
+
       </ul>
     </nav>
   </header>
-<hr class="underline">
+  <hr class="underline">
 </template>
 
 <style scoped>
 .header {
   height: 19vh;
   width: 100vw;
-  }
-  
+}
+
 hr {
   border: none;
   height: 2px;
   background-color: #d9d9d9;
   width: 100%;
 }
+
 .iconsGroup {
   display: flex;
   justify-content: end;
@@ -118,7 +118,8 @@ hr {
   margin: 0 15rem;
   list-style: none;
 }
-.navbar{
+
+.navbar {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -126,26 +127,30 @@ hr {
   margin: 0 15rem;
   transition: 0.5s;
 }
-.navbar__img{
+
+.navbar__img {
   width: 9vw;
-  margin-left: -11rem ;
+  margin-left: -11rem;
   margin-right: 8vw;
   margin-top: -3.5vh;
 }
-.navbar__menu{
+
+.navbar__menu {
   display: flex;
   list-style: none;
   gap: 3rem;
 }
-.navbar__menu--item{
+
+.navbar__menu--item {
   font-size: 1.2rem;
   font-weight: 600;
   letter-spacing: 0.1rem;
-  display: table-cell; 
-  position: relative; 
+  display: table-cell;
+  position: relative;
   padding: 15px 0;
 }
-.navbar__menu--item a{
+
+.navbar__menu--item a {
   color: black;
   text-transform: uppercase;
   text-decoration: none;
@@ -156,7 +161,7 @@ hr {
   transition: 0.3s;
 }
 
-.navbar__menu--item a:after {    
+.navbar__menu--item a:after {
   background: none repeat scroll 0 0 var(--orange);
   bottom: 0;
   content: "";
@@ -167,25 +172,24 @@ hr {
   transition: width 0.3s ease 0s, left 0.3s ease 0s;
   width: 0;
 }
-.navbar__menu--item a:hover{
+
+.navbar__menu--item a:hover {
   color: var(--orange);
 }
-.navbar__menu--item a:hover:after { 
-  width: 100%; 
-  left: 0; 
+
+.navbar__menu--item a:hover:after {
+  width: 100%;
+  left: 0;
 }
 
-@media (max-width: 1000px) {
-  
-}
+@media (max-width: 1000px) {}
 
 @media (max-width: 1500px) {
   .navbar__menu {
     gap: 1rem;
   }
+
   .navbar__menu--item {
     font-size: 1rem;
   }
-}
-
-</style>
+}</style>
