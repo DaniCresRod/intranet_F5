72914a91d8ecafa7aac667cb450d0cf74b9085e7
javier <ertoyos@gmail.com>
2023-10-13 12:11:22 +0200

    add logs view

---
+++ FrontEnd/src/views/Logs.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h2>Historial de solicitudes</h2>
+
+    <div class="grid">
+      <div>
+        <h3>Solicitudes creadas</h3>
+        <v-card v-for="(log, index) in createdLogs" :key="index" class="mb-5 pa-3">
+          <v-card-title class="headline">{{ log.logDate }}</v-card-title>
+          <v-card-text>{{ log.logText }}</v-card-text>
+        </v-card>
+      </div>
+
+      <div>
+        <h3>Solicitudes aceptadas</h3>
+        <v-card v-for="(log, index) in acceptedLogs" :key="index" class="mb-5 pa-3">
+          <v-card-title class="headline">{{ log.logDate }}</v-card-title>
+          <v-card-text>{{ log.logText }}</v-card-text>
+        </v-card>
+      </div>
+
+      <div>
+        <h3>Solicitudes rechazadas</h3>
+        <v-card v-for="(log, index) in rejectedLogs" :key="index" class="mb-5 pa-3">
+          <v-card-title class="headline">{{ log.logDate }}</v-card-title>
+          <v-card-text>{{ log.logText }}</v-card-text>
+        </v-card>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import LogsService from '../services/LogsService';
+
+const logs = ref([]);
+const acceptedLogs = ref([]);
+const rejectedLogs = ref([]);
+const createdLogs = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await LogsService.getAll();
+    console.log(response.data);  
+    logs.value = response.data;
+    acceptedLogs.value = logs.value.filter(log => log.logText.includes('Se ha aceptado'));
+    rejectedLogs.value = logs.value.filter(log => log.logText.includes('Se ha rechazado'));
+    createdLogs.value = logs.value.filter(log => log.logText.includes('Se crea la petición'));
+  } catch (error) {
+    console.error(error);
+  }
+});
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+}
+
+.v-card {
+  margin-top: 20px;
+}
+</style>
