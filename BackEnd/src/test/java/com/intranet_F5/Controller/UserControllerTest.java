875151ad7fc87b57e8441cbe6adb57963471ec69
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-10-16 19:27:23 +0200

    ok UserControllerTest

--- BackEnd/src/test/java/com/intranet_F5/Controller/UserControllerTest.java
+++ BackEnd/src/test/java/com/intranet_F5/Controller/UserControllerTest.java
@@ -8,6 +8,7 @@ import com.intranet_F5.Repository.UserRepository;
 import com.intranet_F5.Services.JwtService;
 import com.intranet_F5.Services.LogService;
 import com.intranet_F5.Services.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,6 +21,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -29,6 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 @AutoConfigureMockMvc
@@ -50,13 +53,19 @@ class UserControllerTest {
     @MockBean
     UserDetailsService userDetailsService;
 
+    String myToken;
+    @BeforeEach
+    public void InitToken(){
+        myToken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPcHRpbXVzIiwiaWF0IjoxNjk3NDc2NDk4LCJleHAiOjE2OTc1NjI4OTh9.XzyGC4g1c9bv0r3oEjk7fZt9xnEOTkpj0je-z8fM2pY";
+    }
+
     @Test
     @WithMockUser(username = "root", roles = "HR")
     void addNewUser() throws Exception {
 
 
         String userJson = "{\"userName\": \"John\", \"userSurName\": \"Doe\", \"userNif\": \"12345\", \"userEmail\": \"john@example.com\", \"userPhone\": \"123456789\", \"userType\": \"Formador\", \"userDept\": \"Pedagógico\", \"userPass\": \"password\"}";
-        String myToken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPcHRpbXVzIiwiaWF0IjoxNjk3NDUxMjk5LCJleHAiOjE2OTc1Mzc2OTl9.UUQ5mWU1UiYCcncLBMEJRYnLJbxxByz96Rd4_chIbts";
+       // String myToken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPcHRpbXVzIiwiaWF0IjoxNjk3NDUxMjk5LCJleHAiOjE2OTc1Mzc2OTl9.UUQ5mWU1UiYCcncLBMEJRYnLJbxxByz96Rd4_chIbts";
 
 
         when(userService.addNewUser(any(UserModel.class))).thenReturn("User added successfully");
@@ -81,7 +90,7 @@ class UserControllerTest {
         List<UserModel> users = new ArrayList<>();
         users.add(myTestUser1);
 
-        String myToken="Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJPcHRpbXVzIiwiaWF0IjoxNjk3NDUxMjk5LCJleHAiOjE2OTc1Mzc2OTl9.UUQ5mWU1UiYCcncLBMEJRYnLJbxxByz96Rd4_chIbts";
+
 
         when(userService.getAllUsers()).thenReturn(users);
 
@@ -107,4 +116,69 @@ class UserControllerTest {
         assertEquals(myTestUser1.getUsername(),  userResponse.getUsername());
     }
 
+    @Test
+    @WithMockUser(username = "root", roles = "HR")
+    void upgradeUser() throws Exception {
+        UserModel newUser = new UserModel(1L, "userName1", "userSurname1", "userNif1", "user1@email.com", "userPhone1", null, null, null, 30, "null", UserModel.UserType.Formador, null, null, null, null);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        //Evita problemas con username y password de UserDetails, asi no da error cuando no los encuentra
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        String myJson=objectMapper.writeValueAsString(newUser);
+
+        when(userService.upgradeUser(any(Long.class), any(UserModel.class))).thenReturn(newUser);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/users/upgrade/{id}", 1L).with(csrf().asHeader())
+                        .content(myJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, myToken )
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userName").value("userName1"));
+    }
+
+    @Test
+    @WithMockUser(username = "root", roles = "HR")
+    void deleteUser() throws Exception {
+        long userId = 1L;
+
+        when(userService.deleteUser(userId)).thenReturn("User deleted successfully");
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/users/delete/{id}", userId).with(csrf().asHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, myToken))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(200, response.getStatus());
+        assertEquals("User deleted successfully", response.getContentAsString());
+    }
+
+    @Test
+    @WithMockUser(username = "root", roles = "HR")
+    void getUserById() throws Exception {
+        long userId = 1L;
+        UserModel myTestUser1 = new UserModel(1L, "userName1", "userSurname1", "userNif1", "user1@email.com", "userPhone1", null, null, null, 30, "null", UserModel.UserType.Formador, null, null, null, null);
+
+        when(userService.getUserById(userId)).thenReturn(myTestUser1);
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/users/{id}", userId).with(csrf().asHeader())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, myToken))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(200, response.getStatus());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        //Evita problemas con username y password de UserDetails, así no da error cuando no los encuentra
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        UserModel userResponse = objectMapper.readValue(response.getContentAsString(), UserModel.class);
+
+        assertEquals(myTestUser1.getUsername(), userResponse.getUsername());
+    }
 }
